e6e1dfd882e6c1160558e03bce2ef5c7fd5689e7
rawand-yezza <rawand.yezza1996@gmail.com>
2023-05-27 11:26:00 +0000

    update run_kube

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,11 +1,11 @@
-kubectl create ns petclinic
-helm create config-server config-server/ --values=./config-server/values.yaml
-helm create config-server discovery-server/ --values=./discovery-server/values.yaml
-helm create customers-service customers-service/ --values=./customers-service/values.yaml
-helm create visits-service visits-service/ --values=./visits-service/values.yaml
-helm create vets-service vets-service/ --values=./vets-service/values.yaml
-helm create api-gateway api-gateway/ --values=./api-gateway/values.yaml
-helm create tracing-server tracing-server/ --values=./tracing-server/values.yaml
-helm create admin-server admin-server/ --values=./admin-server/values.yaml
-helm create grafana-server grafana-server/ --values=./grafana-server/values.yaml
-helm create prometheus-server prometheus-server/ --values=./prometheus-server/values.yaml
+#kubectl install  ns petclinic
+helm install  config-server config-server/ --values=./config-server/values.yaml
+helm install  config-server discovery-server/ --values=./discovery-server/values.yaml
+helm install  customers-service customers-service/ --values=./customers-service/values.yaml
+helm install  visits-service visits-service/ --values=./visits-service/values.yaml
+helm install  vets-service vets-service/ --values=./vets-service/values.yaml
+helm install  api-gateway api-gateway/ --values=./api-gateway/values.yaml
+helm install  tracing-server tracing-server/ --values=./tracing-server/values.yaml
+helm install  admin-server admin-server/ --values=./admin-server/values.yaml
+helm install  grafana-server grafana-server/ --values=./grafana-server/values.yaml
+helm install  prometheus-server prometheus-server/ --values=./prometheus-server/values.yaml
