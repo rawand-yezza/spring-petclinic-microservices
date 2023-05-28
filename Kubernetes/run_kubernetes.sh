23eb3618ef7ae57e3f527bc47c0203d01b4cff0a
rawand-yezza <rawand.yezza1996@gmail.com>
2023-05-28 15:04:23 +0000

    test the deploy

--- Kubernetes/run_kubernetes.sh
+++ Kubernetes/run_kubernetes.sh
@@ -1,15 +1,16 @@
 #create namespace petclinic 
-#kubens petclinic 
-helm upgrade --install config-server ./config-server --values=./config-server/values.yaml 
-helm upgrade --install discovery-server ./discovery-server --values=./discovery-server/values.yaml
-helm upgrade --install customers-server ./customers-server --values=./customers-server/values.yaml
-helm upgrade --install visits-service ./visits-service --values=./visits-service/values.yaml
-helm upgrade --install vets-service ./vets-service --values=./vets-service/values.yaml
-helm upgrade --install api-gateway ./api-gateway --values=./api-gateway/values.yaml
-helm upgrade --install tracing-server ./tracing-server --values=./tracing-server/values.yaml
-helm upgrade --install admin-server ./admin-server --values=./admin-server/values.yaml
-helm upgrade --install grafana-server ./grafana-server --values=./grafana-server/values.yaml
-helm upgrade --install prometheus-server ./prometheus-server --values=./prometheus-server/values.yaml
+
+#kubectl create ns petclinic
+helm upgrade --install config-server config-server --values=./config-server/values.yaml 
+helm upgrade --install discovery-server discovery-server --values=./discovery-server/values.yaml
+helm upgrade --install customers-server customers-server --values=./customers-server/values.yaml
+helm upgrade --install visits-service visits-service --values=./visits-service/values.yaml
+helm upgrade --install vets-service vets-service --values=./vets-service/values.yaml
+helm upgrade --install api-gateway api-gateway --values=./api-gateway/values.yaml
+helm upgrade --install tracing-server tracing-server --values=./tracing-server/values.yaml
+helm upgrade --install admin-server admin-server --values=./admin-server/values.yaml
+helm upgrade --install grafana-server grafana-server --values=./grafana-server/values.yaml
+helm upgrade --install prometheus-server prometheus-server --values=./prometheus-server/values.yaml
 #kubectl create namespace petclinic 
 # kunens petclinic 
 # helm install config-server config-server/ --values=./config-server/values.yaml
