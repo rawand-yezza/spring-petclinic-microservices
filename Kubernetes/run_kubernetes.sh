b40f923edd5194dc106a9db41a808b6377bce246
rawand-yezza <rawand.yezza1996@gmail.com>
2023-05-31 12:04:43 +0000

    tester deploy

--- Kubernetes/run_kubernetes.sh
+++ Kubernetes/run_kubernetes.sh
@@ -1,8 +1,8 @@
 #create namespace petclinic 
 
-kubectl create ns petclinic
+#kubectl create ns petclinic
 helm upgrade --install config-server ./Kubernetes/config-server --values=./Kubernetes/config-server/values.yaml 
-helm upgrade --install discovery-server discovery-server --values=./discovery-server/values.yaml
+helm upgrade --install discovery-server ./Kubernetes/discovery-server --values=./Kubernetes/discovery-server/values.yaml
 helm upgrade --install customers-server customers-server --values=./customers-server/values.yaml
 helm upgrade --install visits-service visits-service --values=./visits-service/values.yaml
 helm upgrade --install vets-service vets-service --values=./vets-service/values.yaml
