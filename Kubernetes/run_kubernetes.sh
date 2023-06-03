6b52b07a0ab4a07ca3c3f2b271fce31cd5b4ba6b
rawand-yezza <rawand.yezza1996@gmail.com>
2023-06-03 14:34:40 +0000

    tester pipline

--- Kubernetes/run_kubernetes.sh
+++ Kubernetes/run_kubernetes.sh
@@ -3,12 +3,12 @@
 kubectl create ns petclinic
 helm upgrade --install config-server ./Kubernetes/config-server --values=./Kubernetes/config-server/values.yaml 
 helm upgrade --install discovery-server ./Kubernetes/discovery-server --values=./Kubernetes/discovery-server/values.yaml
-# helm upgrade --install customers-server customers-server --values=./customers-server/values.yaml
-# helm upgrade --install visits-service visits-service --values=./visits-service/values.yaml
-# helm upgrade --install vets-service vets-service --values=./vets-service/values.yaml
-# helm upgrade --install api-gateway api-gateway --values=./api-gateway/values.yaml
-# helm upgrade --install tracing-server tracing-server --values=./tracing-server/values.yaml
-# helm upgrade --install admin-server admin-server --values=./admin-server/values.yaml
+ helm upgrade --install customers-server ./Kubernetes/customers-server --values=./Kubernetes/customers-server/values.yaml
+ helm upgrade --install visits-service ./Kubernetes/visits-service --values=./Kubernetes/visits-service/values.yaml
+ helm upgrade --install vets-service ./Kubernetes/vets-service --values=./Kubernetes/vets-service/values.yaml
+ helm upgrade --install api-gateway ./Kubernetes/api-gateway --values=./Kubernetes/api-gateway/values.yaml
+ helm upgrade --install tracing-server ./Kubernetes/tracing-server --values=./Kubernetes/tracing-server/values.yaml
+ helm upgrade --install admin-server ./Kubernetes/admin-server --values=./Kubernetes/admin-server/values.yaml
 helm upgrade --install grafana-server ./Kubernetes/grafana-server --values=./Kubernetes/grafana-server/values.yaml
 helm upgrade --install prometheus-server ./Kubernetes/prometheus-server --values=./Kubernetes/prometheus-server/values.yaml
 #kubectl create namespace petclinic 
